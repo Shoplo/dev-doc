142e29a0e46cf9632a783b73fdcf9193b624bfbf
rymopl <lukasz.riemer@gmail.com>
2019-01-15 15:56:57 +0100

    test sites

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,1 +1,6 @@
-export const imports = {}
+export const imports = {
+  'pages/guidelines.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "pages-guidelines" */ 'pages/guidelines.mdx'),
+  'pages/best_practicies.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "pages-best-practicies" */ 'pages/best_practicies.mdx'),
+}
