5f6cccfc92fb68841927c7f19424ded4c7447377
rymopl <lukasz.riemer@gmail.com>
2019-01-15 16:39:30 +0100

    vuepress

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'pages/best_practicies.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "pages-best-practicies" */ 'pages/best_practicies.mdx'),
-  'pages/guidelines.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "pages-guidelines" */ 'pages/guidelines.mdx'),
-}
