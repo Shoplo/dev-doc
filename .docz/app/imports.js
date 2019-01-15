0e6286319652c7b62ab71269262feafaf8e1d90e
rymopl <lukasz.riemer@gmail.com>
2019-01-15 15:58:54 +0100

    docs

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,6 @@
 export const imports = {
-  'pages/guidelines.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "pages-guidelines" */ 'pages/guidelines.mdx'),
   'pages/best_practicies.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "pages-best-practicies" */ 'pages/best_practicies.mdx'),
+  'pages/guidelines.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "pages-guidelines" */ 'pages/guidelines.mdx'),
 }
--- docs/static/js/3.f2dff5c3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"./.docz/app/imports.js":function(n,o,p){"use strict";p.r(o),p.d(o,"imports",function(){return s});var s={}}}]);
-//# sourceMappingURL=3.b64830ffb2db28500ab4.js.map
